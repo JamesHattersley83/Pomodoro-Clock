bb8ad3830590de73f32b93d061277d2f096db63f
jhatts <jhatts@hotmail.co.uk>
2018-11-20 22:02:25 +0000

    added scss styles and javascript file

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// variables
+const session_length = 25;
+const break_length = 5;
+
+// event listeners
+const session_decrement = document.querySelector('#session-decrement');
+
+session_decrement.addEventListener('click', function(){
+    console.log('button clicked');
+})
+    
