a041c1102c88bc906fc63e3f7d8d56ef14916be7
jhatts <jhatts@hotmail.co.uk>
2018-11-20 22:07:30 +0000

    linked app.js to html

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,9 @@ const break_length = 5;
 
 // event listeners
 const session_decrement = document.querySelector('#session-decrement');
+const session_increment = document.querySelector('#session-increment');
 
 session_decrement.addEventListener('click', function(){
     console.log('button clicked');
 })
-    
+
